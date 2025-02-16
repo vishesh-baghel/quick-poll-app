37648330d57b89a165605e2cb48023a4e1b602e8
vishesh <visheshbaghel99@gmail.com>
2025-02-16 11:26:14 +0530

    first commit

---
+++ src/main/java/com/example/quick/poll/app/QuickPollAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.quick.poll.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickPollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickPollAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quick/poll/app/QuickPollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quick.poll.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickPollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
